3ad10dad199a55067a62ea76c6e178de9a553cb6
Andrés Mirás Seoane <101251656+AndresMiras@users.noreply.github.com>
2022-03-23 20:44:52 +0100

    36 correcciones de márgenes
    corrección de estilos

--- Jscript.js
+++ Jscript.js
@@ -78,14 +78,12 @@ function myVisor() {
   let listElements = document.querySelectorAll('.list__button--click');
 
   listElements.forEach(listElements => {
-        listElements.addEventListener('click', ()=>{
-
+        listElements.addEventListener('click', () => {
             listElements.classList.toggle('arrow');
-
             let height = 0;
             let menu = listElements.nextElementSibling;
             if(menu.clientHeight == "0"){
-               height=menu.scrollHeight + 5; 
+               height=menu.scrollHeight; 
             }
 
             menu.style.height = `${height}px`;
